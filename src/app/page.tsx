c39a106515e1afae35085b357d22b3bf100b092c
styamkarnstm <satyamkarnstm@gmail.com>
2025-07-16 17:22:48 +0530

    third

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <h1>hello</h1>
-      <div className="border-3 text-center bg-orange-300 p-5 font-(family-name:Hello Paris) text-5xl font-[Hello Paris] italic font-black ">
+  return (  
+    <div>
+    <div className="border-3 text-center bg-orange-300 p-5 font-(family-name:Hello Paris) text-5xl font-[Hello Paris] italic font-black ">
       <h1>NYVARRA</h1>
     </div>
     <div className="flex m-5 border-3 flex-col  sm:flex-row">
@@ -22,6 +21,7 @@ export default function Home() {
       confidence, style  and  purpose
       </p>
     </div>
-    </div>
+  </div> 
+    
   );
 }
